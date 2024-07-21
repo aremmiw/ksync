525c6c8618dbb5e97baf4ad7e5b5a815560426c7
aremmiw <171315007+aremmiw@users.noreply.github.com>
2024-07-22 06:39:52 +1200

    Clarify username/password length constant

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 
 /* CHANGE THIS! This is the path for the SQLite DB. */
 #define DB_PATH "/var/www/data/ksync.sqlite"
-/* Usernames/passwords must be less than this amount */
+/* Usernames/password lengths must be less than this amount */
 #define MAX_USERPASS_LEN 128+1
 
 #endif
